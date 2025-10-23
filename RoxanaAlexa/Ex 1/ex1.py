f664f20966fbfc5481172581067bdbd200987eeb
Roxana-Maria Alexa <roxana.alexa@ddroidd.com>
2025-10-23 16:14:49 +0300

    Add exercise 3

--- RoxanaAlexa/Ex 1/ex1.py
+++ RoxanaAlexa/Ex 1/ex1.py
@@ -1,3 +1,14 @@
+"""
+Exercise 1
+
+Using pandas, determine the following for the sales.csv data:
+The total revenue for each product (quantity  x price_per_unit).
+The total cost for each product (quantity  x cost_per_unit).
+The profit for each product (total revenue - total cost).
+Finally, find the most profitable product overall
+
+"""
+
 import pandas as pd
 
 df = pd.read_csv("sales.csv")
@@ -8,10 +19,10 @@ df["profit_per_product"] = df["total_revenue"] - df["total_cost"]
 
 print(df["profit_per_product"])
 most_profitable_row = df.sort_values(by="profit_per_product", ascending=False).iloc[0]
-print(f"Cel mai profitabil produs este: {most_profitable_row['product']}")
+print(f"The most profitable single sale is: {most_profitable_row['product']}")
 
 total_profit_by_product = df.groupby("product")["profit_per_product"].sum().sort_values(ascending=False)
 print(total_profit_by_product)
 
 most_profitable_product = total_profit_by_product.index[0]
-print(f"Cel mai profibail produs dintre toate este: {most_profitable_product}")
+print(f"The most profitable product overall is : {most_profitable_product}")
